55a2a7f5f959e7953b9a299e86712ce5cbf81853
Teemu Takaluoma <teemu.takaluoma@arm.com>
2019-07-02 09:19:27 +0300

    Cleanup mbed_cloud_client_user_config file
    Make Blocksize to be 512 KiB and update buffer 1 KiB by default.
    
    This saves some RAM and 512 is going to be the client default
    in future.

--- mbed_cloud_client_user_config.h
+++ mbed_cloud_client_user_config.h
@@ -1,5 +1,5 @@
 // ----------------------------------------------------------------------------
-// Copyright 2016-2017 ARM Ltd.
+// Copyright 2019 ARM Ltd.
 //
 // SPDX-License-Identifier: Apache-2.0
 //
@@ -16,7 +16,6 @@
 // limitations under the License.
 // ----------------------------------------------------------------------------
 
-
 #ifndef MBED_CLOUD_CLIENT_USER_CONFIG_H
 #define MBED_CLOUD_CLIENT_USER_CONFIG_H
 
@@ -24,29 +23,15 @@
 #define MBED_CLOUD_CLIENT_TRANSPORT_MODE_TCP
 #define MBED_CLOUD_CLIENT_LIFETIME              3600
 
-#ifdef __FREERTOS__
-    #define SN_COAP_MAX_BLOCKWISE_PAYLOAD_SIZE       512
-#else
-    #define SN_COAP_MAX_BLOCKWISE_PAYLOAD_SIZE       1024
-#endif
+#define SN_COAP_MAX_BLOCKWISE_PAYLOAD_SIZE       512
 
 /* set flag to enable update support in mbed Cloud client */
-#ifndef __FREERTOS__
-    #define MBED_CLOUD_CLIENT_SUPPORT_UPDATE
-#endif
+#define MBED_CLOUD_CLIENT_SUPPORT_UPDATE
+
 /* set download buffer size in bytes (min. 1024 bytes) */
+#define MBED_CLOUD_CLIENT_UPDATE_BUFFER          1024
 
-/* Use larger buffers in Linux */
-#ifdef __linux__
-#define MBED_CLOUD_CLIENT_UPDATE_BUFFER          (2 * 1024 * 1024)
-#else
-#define MBED_CLOUD_CLIENT_UPDATE_BUFFER          2048
-#endif
-
-/* Developer flags for Update feature */
-#if MBED_CONF_APP_DEVELOPER_MODE == 1
-    #define MBED_CLOUD_DEV_UPDATE_CERT
-    #define MBED_CLOUD_DEV_UPDATE_ID
-#endif /* MBED_CONF_APP_DEVELOPER_MODE */
+#define MBED_CLOUD_DEV_UPDATE_CERT
+#define MBED_CLOUD_DEV_UPDATE_ID
 
 #endif /* MBED_CLOUD_CLIENT_USER_CONFIG_H */
