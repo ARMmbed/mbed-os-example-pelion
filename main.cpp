f22a1c4c35602456220d1a2697a69185bd7db91f
Teemu Takaluoma <teemu.takaluoma@arm.com>
2019-07-04 08:20:50 +0300

    Reduced configuration continued (#1)
    * Simplify trace usage
    
    * Cleanup: remove unnessary carriage returns from printfs
    
    * Remove update authorize
    
    This is default behaviour for Update client.
    
    * Print IP
    
    * Handle fcc_init errors
    
    * Cleanup tracing
    
    * Fix default certificate template
    
    * Cleanup default configurations
    
    SST is enabled by default.
    Keep CEC client disabled also for K66F. No need to have advanced
    features on this example.
    
    * Cleanup printing
    
    * Use same resources as MCCE
    
    * Remove mbed_lib.json
    
    * Remove developer-mode flag
    
    * Implement reset+reboot on key-press 'r'
    
    * Remove configs folder with all content
    
    * Remove the need for PAL config file (requires patch)
    
    * Fix printing of counter
    
    * Remove button + led configs
    
    * Remove old tests
    
    * Add Device Management E2E tests
    
    * Allow reading API key from MBED_CLOUD_SDK_API_KEY variable
    
    * Add entropy injection with hardcoded entropy
    
    * All platforms will use hardcoded entropy injection. This is needed
    only for non-TRNG boards, but for code simplicity, do it for all.
    * Accept also CA_CERT existing error from fcc_developer() API.
    
    * Fix F411RE configuration
    
    * Cleanup mbed_cloud_client_user_config file
    
    Make Blocksize to be 512 KiB and update buffer 1 KiB by default.
    
    This saves some RAM and 512 is going to be the client default
    in future.
    
    * Make INFO the default max for trace
    
    * Remove esp-config as its not needed
    
    * Add deregister (5000/0/1) and refactor shutdown handling
    
    * Seperate functions for deregister callback and deregister handling.
    * Refactored the logic for handling shutdown.
    * Bring down network as part of shutdown.
    
    * Update to MCC 3.3.0 and Mbed OS 5.13.0

--- main.cpp
+++ main.cpp
@@ -23,81 +23,77 @@
 #include "m2mresource.h"                       // Required for M2MResource
 
 #include "mbed-trace/mbed_trace.h"             // Required for mbed_trace_*
-#include "mbed-trace-helper.h"                 // Required for mbed_trace_mutex_*
 
 // Pointers to the resources that will be created in main_application().
 static MbedCloudClient *cloud_client;
-static bool cloud_client_registered = false;
+static bool cloud_client_running = true;
+static NetworkInterface *network = NULL;
+
+// Fake entropy needed for non-TRNG boards. Suitable only for demo devices.
+const uint8_t MBED_CLOUD_DEV_ENTROPY[] = { 0xf6, 0xd6, 0xc0, 0x09, 0x9e, 0x6e, 0xf2, 0x37, 0xdc, 0x29, 0x88, 0xf1, 0x57, 0x32, 0x7d, 0xde, 0xac, 0xb3, 0x99, 0x8c, 0xb9, 0x11, 0x35, 0x18, 0xeb, 0x48, 0x29, 0x03, 0x6a, 0x94, 0x6d, 0xe8, 0x40, 0xc0, 0x28, 0xcc, 0xe4, 0x04, 0xc3, 0x1f, 0x4b, 0xc2, 0xe0, 0x68, 0xa0, 0x93, 0xe6, 0x3a };
 
 static M2MResource* m2m_get_res;
 static M2MResource* m2m_put_res;
 static M2MResource* m2m_post_res;
-
+static M2MResource* m2m_deregister_res;
 
 void print_client_ids(void)
 {
-    printf("Account ID: %s\r\n", cloud_client->endpoint_info()->account_id.c_str());
-    printf("ID: %s\r\n", cloud_client->endpoint_info()->endpoint_name.c_str());
-    printf("Endpoint Name: %s\r\n", cloud_client->endpoint_info()->internal_endpoint_name.c_str());
+    printf("Account ID: %s\n", cloud_client->endpoint_info()->account_id.c_str());
+    printf("Endpoint name: %s\n", cloud_client->endpoint_info()->internal_endpoint_name.c_str());
+    printf("Device ID: %s\n\n", cloud_client->endpoint_info()->endpoint_name.c_str());
 }
 
 void button_press(void)
 {
     m2m_get_res->set_value(m2m_get_res->get_value_int() + 1);
-    printf("\nCounter %" PRId64, m2m_get_res->get_value_int());
+    printf("Counter %" PRIu64 "\n", m2m_get_res->get_value_int());
 }
 
 void put_update(const char* /*object_name*/)
 {
-    printf("PUT update %d\r\n", (int)m2m_put_res->get_value_int());
+    printf("PUT update %d\n", (int)m2m_put_res->get_value_int());
 }
 
-void execute_post(void */*arguments*/)
+void execute_post(void* /*arguments*/)
 {
-    printf("POST executed\r\n");
+    printf("POST executed\n");
+}
+
+void deregister_client(void)
+{
+    printf("Unregistering and disconnecting from the network.\n");
+    cloud_client->close();
+}
+
+void deregister(void* /*arguments*/)
+{
+    printf("POST deregister executed\n");
+    deregister_client();
 }
 
 void client_registered(void)
 {
-    printf("Client registered: \r\n");
+    printf("Client registered.\n");
     print_client_ids();
-    cloud_client_registered = true;
 }
 
 void client_unregistered(void)
 {
-    printf("Client unregistered\r\n");
-    cloud_client_registered = false;
+    printf("Client unregistered.\n");
+    (void) network->disconnect();
+    cloud_client_running = false;
 }
 
 void client_error(int err)
 {
-    printf("client_error(%d) -> %s\r\n", err, cloud_client->error_description());
+    printf("client_error(%d) -> %s\n", err, cloud_client->error_description());
 }
 
 void update_progress(uint32_t progress, uint32_t total)
 {
     uint8_t percent = (uint8_t)((uint64_t)progress * 100 / total);
-    printf("Update progress = %" PRIu8 "%%\r\n", percent);
-}
-
-void update_authorize(int32_t request)
-{
-    switch (request) {
-        case MbedCloudClient::UpdateRequestDownload:
-            printf("Download authorized\r\n");
-            cloud_client->update_authorize(MbedCloudClient::UpdateRequestDownload);
-            break;
-
-        case MbedCloudClient::UpdateRequestInstall:
-            printf("Update authorized -> reboot\r\n");
-            cloud_client->update_authorize(MbedCloudClient::UpdateRequestInstall);
-            break;
-
-        default:
-            printf("update_authorize(%" PRId32 "), unknown request", request);
-            break;
-    }
+    printf("Update progress = %" PRIu8 "%%\n", percent);
 }
 
 int main(void)
@@ -106,22 +102,21 @@ int main(void)
 
     status = mbed_trace_init();
     if (status != 0) {
-        printf("mbed_trace_init() failed with %d\r\n", status);
+        printf("mbed_trace_init() failed with %d\n", status);
         return -1;
     }
 
-    mbed_trace_config_set(TRACE_ACTIVE_LEVEL_INFO); // trace level info
-
-    printf("Init KVStore\r\n");
     // Mount default kvstore
+    printf("Initialize KVStore\n");
     status = kv_init_storage_config();
     if (status != MBED_SUCCESS) {
-        printf("kv_init_storage_config() - failed, status %d\r\n", status);
+        printf("kv_init_storage_config() - failed, status %d\n", status);
         return -1;
     }
 
     // Connect with NetworkInterface
-    NetworkInterface *network = NetworkInterface::get_default_instance();
+    printf("Connect to network\n");
+    network = NetworkInterface::get_default_instance();
     if (network == NULL) {
         printf("Failed to get default NetworkInterface\n");
         return -1;
@@ -131,64 +126,79 @@ int main(void)
         printf("NetworkInterface failed to connect with %d\n", status);
         return -1;
     }
-    printf("Network connected\r\n");
+    printf("Network connected with IP %s\n\n", network->get_ip_address());
 
     // Run developer flow
-    printf("Using developer flow\r\n");
+    printf("Start developer flow\n");
     status = fcc_init();
-    status = fcc_developer_flow();
-    if (status != FCC_STATUS_SUCCESS && status != FCC_STATUS_KCM_FILE_EXIST_ERROR) {
-        printf("fcc_developer_flow() failed with %d\r\n", status);
+    if (status != FCC_STATUS_SUCCESS) {
+        printf("fcc_init() failed with %d\n", status);
         return -1;
     }
 
-    printf("Create resources\r\n");
+    // Inject hardcoded entropy for the device. Suitable only for demo devices.
+    (void) fcc_entropy_set(MBED_CLOUD_DEV_ENTROPY, sizeof(MBED_CLOUD_DEV_ENTROPY));
+    status = fcc_developer_flow();
+    if (status != FCC_STATUS_SUCCESS && status != FCC_STATUS_KCM_FILE_EXIST_ERROR && status != FCC_STATUS_CA_ERROR) {
+        printf("fcc_developer_flow() failed with %d\n", status);
+        return -1;
+    }
+
+    printf("Create resources\n");
     M2MObjectList m2m_obj_list;
 
     // GET resource 3200/0/5501
     m2m_get_res = M2MInterfaceFactory::create_resource(m2m_obj_list, 3200, 0, 5501, M2MResourceInstance::INTEGER, M2MBase::GET_ALLOWED);
     if (m2m_get_res->set_value(0) != true) {
-        printf("m2m_get_res->set_value() failed\r\n");
+        printf("m2m_get_res->set_value() failed\n");
         return -1;
     }
 
-    // PUT resource 3200/0/5500
-    m2m_put_res = M2MInterfaceFactory::create_resource(m2m_obj_list, 3342, 0, 5500, M2MResourceInstance::INTEGER, M2MBase::GET_PUT_ALLOWED);
+    // PUT resource 3201/0/5853
+    m2m_put_res = M2MInterfaceFactory::create_resource(m2m_obj_list, 3201, 0, 5853, M2MResourceInstance::INTEGER, M2MBase::GET_PUT_ALLOWED);
     if (m2m_put_res->set_value(0) != true) {
-        printf("m2m_led_res->set_value() failed\r\n");
+        printf("m2m_led_res->set_value() failed\n");
         return -1;
     }
-    if (m2m_put_res->set_value_updated_function(put_update) != true) { // PUT sets led
-        printf("m2m_put_res->set_value_updated_function() failed\r\n");
-        return -1;
-    }
-
-    m2m_post_res = M2MInterfaceFactory::create_resource(m2m_obj_list, 3342, 0, 5500, M2MResourceInstance::INTEGER, M2MBase::POST_ALLOWED);
-    if (m2m_post_res->set_execute_function(execute_post) != true) { // POST toggles led
-        printf("m2m_post_res->set_execute_function() failed\r\n");
+    if (m2m_put_res->set_value_updated_function(put_update) != true) {
+        printf("m2m_put_res->set_value_updated_function() failed\n");
         return -1;
     }
 
-    printf("Starting Pelion Device Management Client\r\n");
-    cloud_client = new MbedCloudClient(client_registered, client_unregistered, client_error, update_authorize, update_progress);
+    // POST resource 3201/0/5850
+    m2m_post_res = M2MInterfaceFactory::create_resource(m2m_obj_list, 3201, 0, 5850, M2MResourceInstance::INTEGER, M2MBase::POST_ALLOWED);
+    if (m2m_post_res->set_execute_function(execute_post) != true) {
+        printf("m2m_post_res->set_execute_function() failed\n");
+        return -1;
+    }
+
+    // POST resource 5000/0/1 to trigger deregister.
+    m2m_deregister_res = M2MInterfaceFactory::create_resource(m2m_obj_list, 5000, 0, 1, M2MResourceInstance::INTEGER, M2MBase::POST_ALLOWED);
+    if (m2m_deregister_res->set_execute_function(deregister) != true) {
+        printf("m2m_post_res->set_execute_function() failed\n");
+        return -1;
+    }
+
+    printf("Register Pelion Device Management Client\n\n");
+    cloud_client = new MbedCloudClient(client_registered, client_unregistered, client_error, NULL, update_progress);
     cloud_client->add_objects(m2m_obj_list);
     cloud_client->setup(network); // cloud_client->setup(NULL); -- https://jira.arm.com/browse/IOTCLT-3114
 
-    printf("Application loop\r\n");
-    while(1) {
+    while(cloud_client_running) {
         int in_char = getchar();
         if (in_char == 'i') {
             print_client_ids(); // When 'i' is pressed, print endpoint info
             continue;
+        } else if (in_char == 'r') {
+            (void) fcc_storage_delete(); // When 'r' is pressed, erase storage and reboot the board.
+            printf("Storage erased, rebooting the device.\n\n");
+            wait(1);
+            NVIC_SystemReset();
         } else if (in_char > 0 && in_char != 0x03) { // Ctrl+C is 0x03 in Mbed OS and Linux returns negative number
             button_press(); // Simulate button press
             continue;
         }
-        printf("Unregistering\r\n");
-        cloud_client->close();
-        while (cloud_client_registered == true) {
-            wait(1);
-        }
+        deregister_client();
         break;
     }
     return 0;
