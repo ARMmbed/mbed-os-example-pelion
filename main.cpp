50a4e39097ce003d5871736375ae2133a7b82874
Antti Yli-Tokola <antti.yli-tokola@arm.com>
2019-11-25 15:30:28 +0200

    Test CI setup

--- main.cpp
+++ main.cpp
@@ -15,6 +15,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 // ----------------------------------------------------------------------------
+
+
 #ifndef MBED_TEST_MODE
 #include "mbed.h"
 #include "kv_config.h"
